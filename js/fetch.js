ed9ff2f46f178bc7349fce56e606675b0e2e4d39
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-20 17:46:59 +0200

    feat: dynamic url variable for fetch

--- js/fetch.js
+++ js/fetch.js
@@ -3,21 +3,21 @@
  * Fetches the computers from json file
  */
 const getComputers = async () => {
-    return await fetchData("../data/computers.json");
+    return await fetchData("data/computers.json");
 }
 
 /**
  * Fetches bank acount from json file
  */
 const getAccount = async () => {
-    return await fetchData("../data/account.json");
+    return await fetchData("data/account.json");
 }
 
 /**
  * Fetches work account from json file
  */
 const getWork = async () => {
-    return await fetchData("../data/work.json");
+    return await fetchData("data/work.json");
 }
 
 /**
@@ -25,7 +25,8 @@ const getWork = async () => {
  * @param  url 
  */
 const fetchData = async (url) => {
-    const data = await fetch(url);
+    const baseurl = await window.location.origin+window.location.pathname;
+    const data = await fetch(baseurl + url);
     const json_data = await data.json();
     return json_data;
 }
